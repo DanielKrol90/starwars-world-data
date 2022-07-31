badbc10c4e65e8b2288c6992dbcf54f039428f13
Daniel Król <danielkrol90@gmail.com>
2022-07-31 15:03:05 +0200

    hide/show data on planet work

--- src/PlanetsData/PlanetsData.js
+++ src/PlanetsData/PlanetsData.js
@@ -1,20 +1,23 @@
+import { useState } from 'react';
 import "./PlanetsData.css";
 
 const Planets = (props) => {
+
+    const[display, setDisplay] = useState(false)
 
     let planet = props.planets;
 
   return (
     <div className="planet" key={planet.name}>
-      <div className="planetName">Information about planet <span>{planet.name}</span></div>
-      <ul className="listOfPlanets">
+      <div className="planetName" onClick={() => {setDisplay(!display)}}> Information about planet <span>{planet.name}</span></div>
+      {display && <ul className="listOfPlanets">
         <li>Gravity:<span> {planet.gravity}</span></li>
         <li>Climate: <span>{planet.climate}</span></li>
         <li>Diameter: <span> {planet.diameter}</span></li>
         <li>Population: <span>{planet.population}</span></li>
         <li>Rotation period:<span> {planet.rotation_period} </span></li>
         <li>Surface: <span>{planet.surface_water}</span> </li>
-      </ul>
+      </ul>}
     </div>
   );
 
