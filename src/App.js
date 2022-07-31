db077e41b2fe6865c648f52d6af21c2e3fec6d5d
Daniel Król <danielkrol90@gmail.com>
2022-07-31 10:57:11 +0200

    progress on work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [planets, setPlanets] = useState([])
+
+
+
+
+  const getPlanetsData = () => {
+    axios
+      .post('https://swapi.dev/api/planets')
+      .then((req) => {
+        setPlanets(req.data);
+        console.log(req.data);
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err);
+      });
+  };
+
+
+
+
+
+
+  
   return (
     <div className="App">
+      <h1>Star Wars Universe List of Planets</h1>
+        <div className="listPlanets">
+        </div>
+
     </div>
   );
 }
